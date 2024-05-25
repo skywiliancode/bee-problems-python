ed29014f7c5fd67ebbbc6d029a97e64041ae050c
skywilian <codewilian852@gmail.com>
2024-05-24 21:48:01 -0300

    Calculo do salário

---
+++ problems/bee_1008.py
@@ -0,0 +1,8 @@
+NUMBER = int(input())
+HOUR_WORK = float(input())
+MONEY_HOUR = float(input())
+
+SALARY = MONEY_HOUR * HOUR_WORK
+
+print("NUMBER = {}".format(NUMBER))
+print("SALARY = U$ {:.2f}".format(SALARY))
